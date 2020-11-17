c30565b6105276903829f0f96e91797c64b9a14d
Stephanie Wynn <stephsmac@Stephanies-MacBook-Pro-2.local>
2020-11-17 12:19:42 -0800

    adding sounds

---
+++ index.js
@@ -0,0 +1,16 @@
+window.addEventListener("load", () => {
+    const sounds = document.querySelectorAll(".sound");
+    const pads = document.querySelectorAll(".pads div");
+    
+    console.log(sounds)
+
+    //Lets get going with the sound here
+    pads.forEach((pad, index) =>{
+        pad.addEventListener('click', function(){
+            sounds[index].currentTime = 0;
+            sounds[index].play();
+        });
+    });
+});
+
+
