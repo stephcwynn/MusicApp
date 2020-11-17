85908b8349b39be92c7feb4791aaf4f477c4b57c
Stephanie Wynn <stephsmac@Stephanies-MacBook-Pro-2.local>
2020-11-17 13:06:43 -0800

    creating bubbles

--- index.js
+++ index.js
@@ -16,10 +16,21 @@ window.addEventListener("load", () => {
         pad.addEventListener('click', function(){
             sounds[index].currentTime = 0;
             sounds[index].play();
+
+            createBubbles(index);
         });
     });
 
-    
+    //Create a function that makes bubbles
+    const createBubbles = (index) => {
+        const bubble = document.createElement("div");
+        visual.appendChild(bubble);
+        bubble.style.backgroundColor = colors[index];
+        bubble.style.animation = 'jump 1s ease';
+        bubble.addEventListener('animationend', function(){
+            visual.removeChild(this);
+        });
+    };
 });
 
 
