56e19e5fef1794c676d61ad085b3d822c9c368d6
Stephanie Wynn <stephsmac@Stephanies-MacBook-Pro-2.local>
2020-11-20 16:51:31 -0800

    added more buttons

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 window.addEventListener("load", () => {
     const sounds = document.querySelectorAll(".sound");
     const pads = document.querySelectorAll(".pads div");
-    const visual = document.querySelector('.visual');
+    const visual = document.querySelector(".visual");
     const colors = [
         "#60d394", 
         "#d36060", 
         "#c060d3", 
         "#d3d160", 
         "#6860d3", 
-        "#60b2d3"
+        "#60b2d3",
+        "#d36096",
+        "#d3b860",
+        "#a16a84",
+        "#c97507",
     ];
     
     //Lets get going with the sound here
